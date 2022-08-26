be0150923c0fbe3dc780267e1d1f20b7d7d2cc2e
Prashant Maurya <0166621m@gmail.com>
2022-08-27 03:20:03 +0530

    Getting started with backend

---
+++ server.js
@@ -0,0 +1,19 @@
+const express = require('express');
+const products = require('./products');
+
+const app = express();
+
+app.get('/', (req, res) => {
+  res.send('API is running');
+});
+
+app.get('/api/products', (req, res) => {
+  res.json(products);
+});
+
+app.get('/api/product/:id', (req, res) => {
+  const product = products.find((p) => p._id === req.params.id);
+  res.json(product);
+});
+
+app.listen(5000, console.log('Server is running on post 5000'));
