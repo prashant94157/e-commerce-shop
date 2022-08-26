5c5819991ce4ab4c75a361af26f0dfd0c734a405
prashant maurya <0166621m@gmail.com>
2022-08-27 02:24:32 +0530

    Adding product display screen

--- client/src/App.js
+++ client/src/App.js
@@ -1,16 +1,27 @@
 import React from 'react';
 import Footer from './components/Footer';
 import Header from './components/Header';
+import HomeScreen from './screens/HomeScreen';
+import ProductScreen from './screens/ProductScreen';
+
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+
+import { Container } from 'react-bootstrap';
 
 const App = () => {
   return (
-    <>
+    <Router>
       <Header />
-      <main>
-        <h1>Welcome guys</h1>
+      <main className='py-3'>
+        <Container>
+          <Routes>
+            <Route path='/' element={<HomeScreen />} />
+            <Route path='/product/:id' element={<ProductScreen />} />
+          </Routes>
+        </Container>
       </main>
       <Footer />
-    </>
+    </Router>
   );
 };
 
--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,7 +1,37 @@
 import React from 'react';
+import { LinkContainer } from 'react-router-bootstrap';
+import { Navbar, Nav, Container, NavDropdown } from 'react-bootstrap';
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <header>
+      <Navbar bg='dark' variant='dark' expand='lg' collapseOnSelect>
+        <Container>
+          <LinkContainer to='/'>
+            <Navbar.Brand>Jethalal Shop</Navbar.Brand>
+          </LinkContainer>
+          <Navbar.Toggle aria-controls='basic-navbar-nav' />
+          <Navbar.Collapse
+            id='basic-navbar-nav'
+            className='justify-content-end'
+          >
+            <Nav className='ml-auto'>
+              <LinkContainer to='/cart'>
+                <Nav.Link>
+                  <i className='fas fa-shopping-cart'>Cart</i>
+                </Nav.Link>
+              </LinkContainer>
+              <LinkContainer to='/login'>
+                <Nav.Link>
+                  <i className='fas fa-user'>Sign In</i>
+                </Nav.Link>
+              </LinkContainer>
+            </Nav>
+          </Navbar.Collapse>
+        </Container>
+      </Navbar>
+    </header>
+  );
 };
 
 export default Header;
