2a8eec8d5d7e6e1a255cfeb132ef0f5dc2ea8ea5
Prashant Maurya <0166621m@gmail.com>
2022-08-28 18:37:58 +0530

    customised error handling

---
+++ routes/productRoutes.js
@@ -0,0 +1,31 @@
+import express from 'express';
+const router = express.Router();
+import asyncHandler from 'express-async-handler';
+import Product from '../models/productModel.js';
+
+// @desc    Fetch all products
+// @route   GET /api/products
+// @access  public
+router.get(
+  '/',
+  asyncHandler(async (req, res) => {
+    const products = await Product.find();
+    res.json(products);
+  })
+);
+
+// @desc    Fetch product with id
+// @route   GET /api/products/:id
+// @access  public
+router.get(
+  '/:id',
+  asyncHandler(async (req, res) => {
+    const product = await Product.findById(req.params.id);
+    if (!product) {
+      res.status(404);
+      throw new Error('Product not found');
+    } else res.json(product);
+  })
+);
+
+export default router;
